zzygyx9119/book-example

new ft for duplicate items. --ch13l001--

functional_tests/test_list_item_validation.py
@@ -44,3 +44,21 @@ class ItemValidationTest(FunctionalTest):
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
 
+
+    def test_cannot_add_duplicate_items(self):
+        # Edith goes to the home page and starts a new list
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy wellies')
+
+        # She accidentally tries to enter a duplicate item
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        # She sees a helpful error message
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You've already got this in your list"
+        ))
+

yanzhen74/book-example-1

functional_tests/test_list_item_validation.py
@@ -44,3 +44,21 @@ class ItemValidationTest(FunctionalTest):
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
 
+
+    def test_cannot_add_duplicate_items(self):
+        # Edith goes to the home page and starts a new list
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy wellies')
+
+        # She accidentally tries to enter a duplicate item
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        # She sees a helpful error message
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You've already got this in your list"
+        ))
+