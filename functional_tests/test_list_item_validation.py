zzygyx9119/book-example

Start implementing validation FT. --ch11l008--

functional_tests/test_list_item_validation.py
@@ -7,16 +7,22 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
+        self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        )
 
         # She tries again with some text for the item, which now works
+        self.fail('finish this test!')
 
         # Perversely, she now decides to submit a second blank list item
 
         # She receives a similar warning on the list page
 
         # And she can correct it by filling some text in
-        self.fail('write me!')
 

yanzhen74/book-example-1

functional_tests/test_list_item_validation.py
@@ -7,16 +7,22 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
+        self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        )
 
         # She tries again with some text for the item, which now works
+        self.fail('finish this test!')
 
         # Perversely, she now decides to submit a second blank list item
 
         # She receives a similar warning on the list page
 
         # And she can correct it by filling some text in
-        self.fail('write me!')
 